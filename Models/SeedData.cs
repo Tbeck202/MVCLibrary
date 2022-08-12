2e51bc9b31aa429e45b63ab2cbd4692f6568f1bd
Tyler Beck <tbeck202@gmail.com>
2022-08-12 14:22:50 -0700

    Add project files.

---
+++ Models/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if(context.Book.Any())
+                {
+                    return;
+                }
+                context.AddRange(
+                    new Book { Title = "Lord of the Rings: Fellowship of the Ring", CallNumber = "LOR 123" },
+                    new Book { Title = "Fight Club", CallNumber = "FIG 123" },
+                    new Book { Title = "Learning C#", CallNumber = "LEA 123" }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
